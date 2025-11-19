0ef1709a908256a8572c0902955aaddeca43762c
Zantoan <phamvantoandtb@gmail.com>
2025-11-19 17:31:24 +0700

    second commit

--- server/server.js
+++ server/server.js
@@ -19,6 +19,6 @@ app.use("/api/clerk", clerkWebhook);
 
 app.get('/',(req, res)=> res.send("API is working "))
 
-const PORT = process.env.PORT || 3000;
+/*const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});*/
